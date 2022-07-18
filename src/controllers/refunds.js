60382267f3630a63bbfc23ff845a6faddea81f45
Tushar Joshi <103056758+Tushar-Centillion@users.noreply.github.com>
2022-07-18 18:58:14 +0530

    add some shanges in refund

--- src/controllers/refunds.js
+++ src/controllers/refunds.js
@@ -11,26 +11,93 @@ const {
 
 const CreateRefund = async (req, res) => {
 	try {
-		const instance = new Razorpay({
-			key_id: process.env.RAZORPAY_KEY_ID, // YOUR RAZORPAY KEY
-			key_secret: process.env.RAZORPAY_KEY_SECRET, // YOUR RAZORPAY SECRET
-		});
-		const { paymentId, amount, note } = req.body;
 		console.log('req.body', req.body);
-		const refund = instance.payments.refund(paymentId, {
-			amount: Number(amount) * 100,
-			speed: 'normal',
-			notes: {
-				notes_key_1: note,
-			},
-			receipt: 'Receipt No. 31',
-		});
-
+		if (req.body) {
+			const { amount, paymentObject } = req.body;
+			var paymentId = paymentObject[0].razorpayPaymentId;
+			console.log('paymentId', paymentId);
+			var payload = {
+				amount: Number(amount),
+			};
+			let RefundUrl = `https://api.razorpay.com/v1/payments/${paymentId}/refund`;
+			const client = axios.create({
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Basic ' + process.env.BASE64_KEYANDSECRET,
+				},
+			});
+			const { data } = await client.post(RefundUrl, payload);
+			console.log('data', data);
+			if (data) {
+				var payloadRefund = {
+					userId: req.body.userId,
+					orderId: req.body.orderId,
+					paymentId: req.body.paymentObject[0].id,
+					refundDate: new Date(),
+					razorpayRefundId: data.id ? data.id : '',
+					entity: data.id,
+					amount: data.amount,
+					currency: data.currency,
+					razorpayPayment_Id: req.body.paymentObject[0].razorpayPaymentId,
+					note: req.body.note,
+					status: data.status ? data.status : '',
+					batch_Id: data.batch_Id ? data.batch_Id : '',
+					speed_Requested: data.speed_Requested ? data.speed_Requested : '',
+				};
+				console.log('payloadRefund', payloadRefund);
+				let AspRefundUrl = `http://143.110.186.132:1454/api/Refund/CreateRefund`;
+				const clientasp = axios.create({
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				});
+				const dataasp = await clientasp.post(AspRefundUrl, payloadRefund);
+				console.log('dataasp', dataasp);
+				if (dataasp) res.json(dataasp);
+			}
+		}
 		// console.log('refund', refund);
 	} catch (error) {
-		// console.log('error', error);
+		console.log('error', error);
+		res.status(500).send(error);
+	}
+};
+const getByidRefund = async (req, res) => {
+	try {
+		console.log('req.body', req.body);
+
+		if (req.body.paymentId) {
+			let AspRefundUrl = `http://143.110.186.132:1454/api/Refund/GetRefundByPaymentId/${req.body.paymentId}`;
+			const clientasp = axios.create({
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			const GetData = await clientasp.get(AspRefundUrl);
+			if (GetData.data.status) {
+				var { razorpayRefundId } = GetData.data?.data;
+				console.log('razorpayRefundId', razorpayRefundId);
+				var getRefund = `https://api.razorpay.com/v1/refunds/${razorpayRefundId}`;
+				const Refund = axios.create({
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: 'Basic ' + process.env.BASE64_KEYANDSECRET,
+					},
+				});
+				const { data } = await Refund.get(getRefund);
+				var payload = {
+					...data,
+					price: data.amount / 100,
+				};
+				res.status(200).send(payload);
+			} else {
+				res.status(500).send('order recode not found!');
+			}
+		}
+	} catch (error) {
+		console.log('error', error);
 		res.status(500).send(error);
 	}
 };
 
-export default { CreateRefund };
+export default { CreateRefund, getByidRefund };
--- src/routes/refunds.js
+++ src/routes/refunds.js
@@ -3,4 +3,5 @@ import { RefundController } from '../controllers';
 import { validator } from '../validation';
 const router = express.Router();
 router.post('/CreateRefundProcess', RefundController.CreateRefund);
+router.post('/getByidRefund', RefundController.getByidRefund);
 module.exports = router;
